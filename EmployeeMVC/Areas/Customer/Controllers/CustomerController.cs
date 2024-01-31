9f4bfe2e6cf7ca8566e42c9d5db110353104e1d3
utsav <utsavadhikari452@gmail.com>
2024-01-31 15:49:19 +0545

    JSon Get success

--- EmployeeMVC/Areas/Customer/Controllers/CustomerController.cs
+++ EmployeeMVC/Areas/Customer/Controllers/CustomerController.cs
@@ -6,9 +6,24 @@ namespace EmployeeMVC.Areas.Customer.Controllers
     [Area("Customer")]
     public class CustomerController : Controller
     {
-        public IActionResult Customer()
+        public JsonResult Customer()
         {
-            return View();
+            Models.Customer customer = new Models.Customer()
+            {
+
+                Id = 1,
+                Name = "Test",
+                Age = 28,
+            };
+
+            /* var Json = JsonConvert.SerializeObject(customer);
+             return Json("", JsonRequestBehavior.AllowGet);*/
+
+            var jsonResult = Json(customer);
+            return Json(jsonResult);
+           
         }
+
+       
     }
 }
--- EmployeeMVC/Models/Customer.cs
+++ EmployeeMVC/Models/Customer.cs
@@ -6,11 +6,6 @@
         public string Name { get; set; }
         public int Age { get; set; }
 
-        public Customer(int id, string name, int age)
-        {
-            Id = id;
-            Name = name;
-            Age = age;
-        }
+       
     }
 }
