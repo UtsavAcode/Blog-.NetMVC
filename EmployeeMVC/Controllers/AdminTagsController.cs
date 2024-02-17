32e7ed0415e897bf578bf58e85d63328d1bd0cb3
utsav <utsavadhikari452@gmail.com>
2024-02-17 22:48:26 +0545

    Submit success

--- EmployeeMVC/Controllers/AdminTagsController.cs
+++ EmployeeMVC/Controllers/AdminTagsController.cs
@@ -1,12 +1,39 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EmployeeMVC.Data;
+using EmployeeMVC.Models.Domain;
+using EmployeeMVC.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EmployeeMVC.Controllers
 {
     public class AdminTagsController : Controller
     {
+        private readonly ApplicationDbContext _applicationDbContext;
+        public AdminTagsController( ApplicationDbContext applicationDbContext)
+        {
+            this._applicationDbContext = applicationDbContext;
+        }
+
+
+        [HttpGet]
         public IActionResult Add()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddTagRequest addTagRequest)
+        {
+            //Mappint the Models.Domain.Tag entity to AddTagRequest Model
+
+            var tag = new Tag()
+            {
+
+                Name = addTagRequest.Name,
+                DisplayName = addTagRequest.DisplayName,
+            };
+
+            _applicationDbContext.Tags.Add(tag);
+            return View("Add");
         }
     }
 }
