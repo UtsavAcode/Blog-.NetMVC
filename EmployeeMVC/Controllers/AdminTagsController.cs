a4cfeb156cf2d89f6591bb24b1b265979162474b
utsav <utsavadhikari452@gmail.com>
2024-02-19 12:06:40 +0545

    Made the Edit and changes features.

--- EmployeeMVC/Controllers/AdminTagsController.cs
+++ EmployeeMVC/Controllers/AdminTagsController.cs
@@ -23,7 +23,7 @@ namespace EmployeeMVC.Controllers
         [HttpPost]
         public IActionResult Add(AddTagRequest addTagRequest)
         {
-            //Mappint the Models.Domain.Tag entity to AddTagRequest Model
+            //Mapping the Models.Domain.Tag entity to AddTagRequest Model
 
             var tag = new Tag()
             {
@@ -42,6 +42,55 @@ namespace EmployeeMVC.Controllers
         {
             var tag = _applicationDbContext.Tags.ToList();
             return View(tag);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(Guid id)
+        {
+            var tag = _applicationDbContext.Tags.FirstOrDefault(x => x.Id == id);
+
+            //Checking if the tag is null or not.
+           if(tag != null)
+            {
+                var editTagRequest = new EditTagRequest()
+                {
+                    Id = tag.Id,
+                    Name = tag.Name,
+                    DisplayName = tag.DisplayName,
+
+                };
+
+                return View(editTagRequest);
+            }
+
+            return View(null);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(EditTagRequest editTagRequest)
+        {
+            //The EditTagRequest must be changed to the Tag Domain model.
+            //this is because of the entity framework only deals with main domain and not the view Model because
+            //The MainDomain deals with the database.
+            var tag = new Tag()
+            {
+                Id = editTagRequest.Id,
+                Name = editTagRequest.Name,
+                DisplayName = editTagRequest.DisplayName,
+            };
+
+            //Finding and storing the required tag into variable the existingTag.
+            var existingTag = _applicationDbContext.Tags.Find(tag.Id);
+
+            if (existingTag !=null)
+            {
+                existingTag.Name = tag.Name;
+                existingTag.DisplayName = tag.DisplayName;
+
+                _applicationDbContext.SaveChanges();
+                return RedirectToAction("List");
+            }
+            return RedirectToAction("Edit");
         }
     }
 }
