e8f8d201a8d72e07728f119e4ba88743015ee7b4
utsav <utsavadhikari452@gmail.com>
2024-02-19 22:44:56 +0545

    Created posts controller and the corresponding View.

---
+++ EmployeeMVC/Controllers/AdminBlogPostsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeMVC.Controllers
+{
+    public class AdminBlogPostsController : Controller
+    {
+       public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
