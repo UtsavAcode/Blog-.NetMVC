9a424cb956c8f8a3337e0c6f3a73803c09ab8bf3
utsav <utsavadhikari452@gmail.com>
2024-02-20 12:49:08 +0545

    Adding Blog Post using Tags half achieved.

--- EmployeeMVC/Controllers/AdminBlogPostsController.cs
+++ EmployeeMVC/Controllers/AdminBlogPostsController.cs
@@ -1,10 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EmployeeMVC.Models.ViewModels;
+using EmployeeMVC.Repository.Interface;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace EmployeeMVC.Controllers
 {
     public class AdminBlogPostsController : Controller
     {
-       public IActionResult Add()
+        private readonly ITagRepository tagRepository;
+
+        public AdminBlogPostsController(ITagRepository tagRepository)
+        {
+            this.tagRepository = tagRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Add()
+        {
+            var tags = await tagRepository.GetAllAsync();
+
+            var model = new AddBlogPostRequest() {
+
+                Tags = tags.Select(x => new SelectListItem { Text = x.Name, Value = x.Id.ToString() })
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddBlogPostRequest addBlogPostRequest)
         {
             return View();
         }
