572af13c7e9ff6c131878ee8fa986f408ffbebfd
utsav <utsavadhikari452@gmail.com>
2024-02-22 13:27:28 +0545

    Image Controller all setup.

--- EmployeeMVC/Controllers/ImagesController.cs
+++ EmployeeMVC/Controllers/ImagesController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using EmployeeMVC.Repository.Interface;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
 
 namespace EmployeeMVC.Controllers
 {
@@ -7,16 +9,29 @@ namespace EmployeeMVC.Controllers
     [ApiController]
     public class ImagesController : ControllerBase
     {
+        private readonly IImageRepository imageRepository;
+
         //The method below wil call a repository and through the repository we will call a third party API service which is
         //Cloudinary
+        public ImagesController(IImageRepository imageRepository)
+        {
+            this.imageRepository = imageRepository;
+        }
 
-     
 
         [HttpPost]
         public async Task<IActionResult> UploadAsync(IFormFile file)
         {
             //call the repository
 
+            var imageUrl = await imageRepository.UploadAsync(file);
+
+            if(imageUrl == null)
+            {
+                return Problem("Something is wrong!!", null, (int)HttpStatusCode.InternalServerError);
+            }
+
+            return new JsonResult(new { link = imageUrl });
         }
     }
 }
--- EmployeeMVC/Program.cs
+++ EmployeeMVC/Program.cs
@@ -13,6 +13,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 
 builder.Services.AddScoped<ITagRepository, TagRepository>();
 builder.Services.AddScoped<IBlogPostRepository, BlogPostRepository>();
+builder.Services.AddScoped<IImageRepository, CloudinaryImageRepository>();
 
 var app = builder.Build();
 
