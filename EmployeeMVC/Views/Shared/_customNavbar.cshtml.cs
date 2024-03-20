80aa3ed3c8db9ce55c6ce326f78a4da1a17dbf16
utsav <utsavadhikari452@gmail.com>
2024-03-20 21:11:08 +0545

    ..

---
+++ EmployeeMVC/Views/Shared/_customNavbar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EmployeeMVC.Views.Shared
+{
+    public class _customNavbarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
